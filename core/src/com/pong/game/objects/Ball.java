3a8fb0942a5b0ae18def4896931bec2703f76dea
NicePixel <nicecubedude@gmail.com>
2014-08-14 17:29:23 +0200

    Added ball rotation, ball speed now changes and commented unimportant code in ball object

--- core/src/com/pong/game/objects/Ball.java
+++ core/src/com/pong/game/objects/Ball.java
@@ -10,11 +10,16 @@ import com.pong.game.GDXPong;
 
 public class Ball {
 	// FIELDS
-	private float speedX = 6;
-	private float speedY = 6;
+	private final float speedX_init = 6;
+	private final float speedY_init = 6;
+	private float speedX = speedX_init;
+	private float speedY = speedY_init;
 
 	private Vector2 pos; // The position
 	private Sprite sprite; // The sprite
+
+	private float rotation;
+	private float rotationSpeed;
 
 	private Rectangle bounds;
 
@@ -24,15 +29,20 @@ public class Ball {
 		// Set the visuals
 		sprite = new Sprite(new Texture(Gdx.files.internal("ball.png")));
 		sprite.setSize(16, 16);
+		sprite.setOriginCenter();
 
 		pos = new Vector2(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
 				Gdx.graphics.getHeight() / 2 - sprite.getHeight() / 2);
 
 		bounds = new Rectangle();
+		
+		rotation = 0f;
+		rotationSpeed = 2f;
 	}
 
 	// Render method
 	public void render(SpriteBatch batch) {
+		sprite.setRotation(rotation);
 		sprite.setPosition(pos.x, pos.y);
 		sprite.draw(batch);
 	}
@@ -67,31 +77,45 @@ public class Ball {
 		}
 
 		bounds.set(pos.x, pos.y, sprite.getWidth(), sprite.getHeight());
-
+		
+		// Handle collision with players
 		if (bounds.overlaps(p1.getBounds())) {
 			if (Math.abs(speedX) != speedX) {
+				
+				speedX += (speedX > 0?1:-1) * 0.2f;
 				speedX = -speedX;
+				
 				System.out.println("--> hit p1");
+				rotationSpeed = -rotationSpeed;
+				
 			}
 		} else if (bounds.overlaps(p2.getBounds())) {
 			if (Math.abs(speedX) == speedX) {
+				
+				speedX += (speedX > 0?1:-1) * 0.2f;
 				speedX = -speedX;
+				
 				System.out.println("--> hit p2");
+				rotationSpeed = -rotationSpeed;
+				
 			}
 		}
+		rotation += rotationSpeed;
 		
 	}
 
-	private void resetBall(int i) {
-		if(i == 1){
-			if(speedX == Math.abs(speedX)){
-				speedX = -speedX;
-			}
-		}else if(i == 2){
-			if(speedX != Math.abs(speedX)){
-				speedX = -speedX;
-			}
-		}
+	private void resetBall(int winPlayer) {
+		
+//		if(winPlayer == 2){
+//			if(speedX == Math.abs(speedX)){
+//				speedX = -speedX;
+//			}
+//		}else if(winPlayer == 1){
+//			if(speedX != Math.abs(speedX)){
+//				speedX = -speedX;
+//			}
+//		}
+		
 		pos.set(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
 				Gdx.graphics.getHeight() / 2 - sprite.getHeight() / 2);
 
