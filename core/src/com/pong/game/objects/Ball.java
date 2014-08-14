c47a5b9641dcd8b4dd5e73e6eef9cff720d514cf
NicePixel <nicecubedude@gmail.com>
2014-08-14 17:51:40 +0200

    Added cooldown to the ball

--- core/src/com/pong/game/objects/Ball.java
+++ core/src/com/pong/game/objects/Ball.java
@@ -10,11 +10,14 @@ import com.pong.game.GDXPong;
 
 public class Ball {
 	// FIELDS
-	private final float speedX_init = 6;
-	private final float speedY_init = 6;
+	private final float speedX_init = 6; // Ball's initial X speed
+	private final float speedY_init = 6; // Ball's initial Y speed
 	private float speedX = speedX_init;
 	private float speedY = speedY_init;
-
+	
+	private final float ballCooldown = 3f; // How much time ball _waits_ to be launched
+	private float currentBallCooldown = 0f; // How much time ball _waited_ to be launched
+	
 	private Vector2 pos; // The position
 	private Sprite sprite; // The sprite
 
@@ -29,8 +32,8 @@ public class Ball {
 		// Set the visuals
 		sprite = new Sprite(new Texture(Gdx.files.internal("ball.png")));
 		sprite.setSize(16, 16);
-		sprite.setOriginCenter();
-
+//		sprite.setOriginCenter();
+		
 		pos = new Vector2(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
 				Gdx.graphics.getHeight() / 2 - sprite.getHeight() / 2);
 
@@ -49,75 +52,70 @@ public class Ball {
 
 	// Update method
 	public void update(Player p1, Player p2) {
+		if(currentBallCooldown < ballCooldown){
+			currentBallCooldown += Gdx.graphics.getDeltaTime();
+		}else{
 
-		pos.x += speedX;
-		pos.y += speedY;
-
-		// Handle collision
-		// Up
-		if (pos.y + speedY + sprite.getHeight() > Gdx.graphics.getHeight()) {
-			if(Math.abs(speedY) == speedY)
-				speedY = -speedY;
-		}
-		// Down
-		else if (pos.y - speedY - sprite.getHeight() < 0) {
-			if(Math.abs(speedY) != speedY)
-				speedY = -speedY;
-		}
-		
-		// Right
-		if (pos.x + speedX + sprite.getWidth() > Gdx.graphics.getWidth()) {
-			GDXPong.setScore(1);
-			resetBall(2);
-		}
-		// Left
-		else if (pos.x + speedX + sprite.getWidth() < 0) {
-			GDXPong.setScore(2);
-			resetBall(1);
-		}
-
-		bounds.set(pos.x, pos.y, sprite.getWidth(), sprite.getHeight());
-		
-		// Handle collision with players
-		if (bounds.overlaps(p1.getBounds())) {
-			if (Math.abs(speedX) != speedX) {
-				
-				speedX += (speedX > 0?1:-1) * 0.2f;
-				speedX = -speedX;
-				
-				System.out.println("--> hit p1");
-				rotationSpeed = -rotationSpeed;
-				
+			pos.x += speedX;
+			pos.y += speedY;
+			
+			// Handle collision
+			// Up
+			if (pos.y + speedY + sprite.getHeight()/2 > Gdx.graphics.getHeight()) {
+				if(Math.abs(speedY) == speedY)
+					speedY = -speedY;
 			}
-		} else if (bounds.overlaps(p2.getBounds())) {
-			if (Math.abs(speedX) == speedX) {
-				
-				speedX += (speedX > 0?1:-1) * 0.2f;
-				speedX = -speedX;
-				
-				System.out.println("--> hit p2");
-				rotationSpeed = -rotationSpeed;
-				
+			// Down
+			else if (pos.y - speedY - sprite.getHeight()/2 < 0) {
+				if(Math.abs(speedY) != speedY)
+					speedY = -speedY;
 			}
+			
+			// Right
+			if (pos.x + speedX + sprite.getWidth()/2 > Gdx.graphics.getWidth()) {
+				GDXPong.setScore(1);
+				resetBall();
+			}
+			// Left
+			else if (pos.x + speedX + sprite.getWidth()/2 < 0) {
+				GDXPong.setScore(2);
+				resetBall();
+			}
+			
+			bounds.set(pos.x, pos.y, sprite.getWidth(), sprite.getHeight());
+			
+			// Handle collision with players
+			if (bounds.overlaps(p1.getBounds())) {
+				if (Math.abs(speedX) != speedX) {
+					
+					speedX += (speedX > 0?1:-1) * 0.2f;
+					speedX = -speedX;
+					
+					System.out.println("--> hit p1");
+					rotationSpeed = -rotationSpeed;
+					
+				}
+			} else if (bounds.overlaps(p2.getBounds())) {
+				if (Math.abs(speedX) == speedX) {
+					
+					speedX += (speedX > 0?1:-1) * 0.2f;
+					speedX = -speedX;
+					
+					System.out.println("--> hit p2");
+					rotationSpeed = -rotationSpeed;
+					
+				}
+			}
+			rotation += rotationSpeed;
+			
 		}
-		rotation += rotationSpeed;
-		
 	}
 
-	private void resetBall(int winPlayer) {
-		
-//		if(winPlayer == 2){
-//			if(speedX == Math.abs(speedX)){
-//				speedX = -speedX;
-//			}
-//		}else if(winPlayer == 1){
-//			if(speedX != Math.abs(speedX)){
-//				speedX = -speedX;
-//			}
-//		}
-		
-		pos.set(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
-				Gdx.graphics.getHeight() / 2 - sprite.getHeight() / 2);
+	private void resetBall() {
+		currentBallCooldown = 0f;
+
+		pos = new Vector2(Gdx.graphics.getWidth() / 2 - sprite.getWidth()/2,
+				Gdx.graphics.getHeight() / 2 - sprite.getHeight()/2);
 
 	}
 }
