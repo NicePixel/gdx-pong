a059ef8c1be1546436a3ebb6f8455ebf3bf24d35
NicePixel <nicecubedude@gmail.com>
2014-08-14 17:11:41 +0200

    ball speed now changed and ball now how rotation

--- core/src/com/pong/game/objects/Ball.java
+++ core/src/com/pong/game/objects/Ball.java
@@ -15,6 +15,9 @@ public class Ball {
 
 	private Vector2 pos; // The position
 	private Sprite sprite; // The sprite
+	
+	private float rotation = 0f;
+	private float rotationSpeed = 2f;
 
 	private Rectangle bounds;
 
@@ -23,7 +26,8 @@ public class Ball {
 
 		// Set the visuals
 		sprite = new Sprite(new Texture(Gdx.files.internal("ball.png")));
-		sprite.setSize(16, 16);
+		sprite.setSize(sprite.getTexture().getWidth(), sprite.getTexture().getHeight());
+		sprite.setOriginCenter();
 
 		pos = new Vector2(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
 				Gdx.graphics.getHeight() / 2 - sprite.getHeight() / 2);
@@ -33,6 +37,7 @@ public class Ball {
 
 	// Render method
 	public void render(SpriteBatch batch) {
+		sprite.setRotation(rotation);
 		sprite.setPosition(pos.x, pos.y);
 		sprite.draw(batch);
 	}
@@ -65,20 +70,49 @@ public class Ball {
 			GDXPong.setScore(2);
 			resetBall(1);
 		}
-
+		
+		// reset the bounds
 		bounds.set(pos.x, pos.y, sprite.getWidth(), sprite.getHeight());
 
+		// Handle collision with players
+		// p1
 		if (bounds.overlaps(p1.getBounds())) {
+			
 			if (Math.abs(speedX) != speedX) {
+				speedX -= 0.2f;
 				speedX = -speedX;
+				
+				if(pos.y > p1.getPos().y + p1.getBounds().getHeight()/2){
+					if(speedY != Math.abs(speedY)){
+						speedY += 0.05f;
+						speedY = - speedY;
+					}
+				}
+				
+				rotationSpeed = -rotationSpeed;
 				System.out.println("--> hit p1");
 			}
-		} else if (bounds.overlaps(p2.getBounds())) {
+			
+		}
+		// p2
+		else if (bounds.overlaps(p2.getBounds())) {
+			
 			if (Math.abs(speedX) == speedX) {
+				speedX += 0.2f;
 				speedX = -speedX;
-				System.out.println("--> hit p2");
+				
+				if(pos.y > p2.getPos().y + p2.getBounds().getHeight()/2){
+					if(speedY != Math.abs(speedY)){
+						speedY = - speedY;
+					}
+				}
+				
+				rotationSpeed = -rotationSpeed;
+				System.out.println("--> hit p2 ");
 			}
 		}
+		
+		rotation += rotationSpeed;
 		
 	}
 
